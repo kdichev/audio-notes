b9712067ff2613389d384bb0be1cd56f744bb7c6
Konstantin-home <kd@freetrailer.dk>
2017-02-19 13:23:47 +0100

    add Controlls component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Controlls from './components/Controlls.js'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Controlls />
       </div>
     );
   }
---
+++ src/components/Controlls.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+
+export default class Controlls extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+
+  render() {
+    return (
+      <div>
+      hello
+      </div>
+    );
+  }
+}
